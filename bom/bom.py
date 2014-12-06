9b3162a8e93ed0f74f005dab33b254906b554fae
Justin Cano <jcano001@ucr.edu>
2014-12-05 17:46:47 -1000

    initial commit

---
+++ bom/bom.py
@@ -0,0 +1,21 @@
+#!/usr/bin python
+
+"""
+Box Office Mojo API
+Unofficial Python API for Box Office Mojo API.
+
+Author: Justin Cano
+Email: jcano001@ucr.edu
+License: MIT
+"""
+import requests
+from bs4 import BeautifulSoup
+from constants import BASE_URL, WEEKEND_CHART
+
+def get_soup(page=WEEKEND_CHART):
+	content = requests.get('%s/%s' % (BASE_URL, page)).text
+	print BeautifulSoup(content)
+
+
+if __name__ == '__main__':
+	get_soup()
---
+++ bom/constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin python
+
+BASE_URL = 'http://boxofficemojo.com'
+WEEKEND_CHART = 'weekend/chart'
+# seconds to sleep between 2 consecutive page requests
+INTERVAL_BETWEEN_REQUESTS = 1
