0eaaab71e08fcf04826448df2eaecbe86f591360
Justin Cano <jcano001@ucr.edu>
2014-12-11 17:09:04 -0800

    added movie_id attribute to Movie class

--- bom/bom.py
+++ bom/bom.py
@@ -24,6 +24,13 @@ class BOM(object):
 	The class that parses the BoxOfficeMojo page, and builds up all the movies
 	"""
 
+	def _get_movie_id(self, movie_id_link):
+		"""
+		Returns the id from movie_id_link
+		"""
+		return re.sub('\/movies\/\?id\=', '', movie_id_link)
+
+
 	def get_movies(self, chart=WEEKEND_CHART, limit=10):
 		"""
 		Yields a list of box office movies from chart of BoxOfficeMojo
@@ -43,11 +50,12 @@ class BOM(object):
 			attrs = m.findChildren('td')
 			rank = int(attrs[0].string)
 			title = str(attrs[2].string)
+			movie_id = self._get_movie_id(str(attrs[2].a['href']))
 			studio = str(attrs[3].string)
 			gross_str = str(attrs[4].string)
 			gross_int = int(gross_str[1:].replace(',',''))
 
-			movie = Movie(rank, title, studio, gross_str, gross_int)
+			movie = Movie(movie_id, rank, title, studio, gross_str, gross_int)
 			yield movie
 			movies_found += 1
 
@@ -60,14 +68,16 @@ class Movie(object):
 	"""
 	Movie class that represents a movie on BoxOfficeMojo
 	"""
-	pass
-	def __init__(self, rank, title, studio, gross_str, gross_int):
+	def __init__(self, movie_id, rank, title, studio, gross_str, gross_int):
+		self.movie_id = movie_id # the movie's ID on BoxOfficeMojo.com
 		self.rank = rank # this week's rank
 		self.title = title # the title of the movie
 		self.studio = studio # the movie's producing studio
 		self.gross_str = gross_str # movie's gross income (str)
 		self.gross_int = gross_int # movie's gross income (int)
 
+	def trend(self):
+		pass
 
 
 
